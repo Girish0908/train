95162bd6dfe8efdb631d93e7a70c48e2c260d615
girish0908 <girish0908>
2023-10-14 03:07:22 -0500

    commit 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         commit2 SEdit <code>src/App.js</code> and save to reload.
+         commit3 SEdit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
