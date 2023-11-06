a163af7fc35cf40a0e1e8f5735de251e9c9fcb5a
girish0908 <girish0908>
2023-11-06 11:11:48 -0600

    89

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-    SEdit <code>src/App.js</code> and save to reload.
+    SEdit 56 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
